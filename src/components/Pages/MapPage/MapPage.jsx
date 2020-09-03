9fe4d4018f5eff23d772a6aaa776633172c27cfe
Grenvals <grenvals@gmail.com>
2020-09-03 21:46:58 +0300

    add leaflet, react=leaflet, base map

--- src/components/Pages/MapPage/MapPage.jsx
+++ src/components/Pages/MapPage/MapPage.jsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import L from 'leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
 import './MapPage.scss';
 
+L.Icon.Default.imagePath = 'https://unpkg.com/leaflet@1.7.1/dist/images/';
+
 const MapPage = () => {
-  return <div className="map">Map</div>;
+  const mapRef = useRef();
+  useEffect(() => {
+    setInterval(() => {
+      if (mapRef.current) {
+        mapRef.current.leafletElement.invalidateSize();
+      }
+    }, 1000);
+  }, [mapRef]);
+
+  const state = {
+    lat: 50.440864,
+    lng: 30.515664,
+    zoom: 10,
+  };
+
+  const center = [state.lat, state.lng];
+
+  return (
+    <div className="map">
+      <Map zoom={state.zoom} center={center} ref={mapRef}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={center}>
+          <Popup>
+            <p>MainText</p>
+            <p>MainText</p>
+          </Popup>
+        </Marker>
+      </Map>
+    </div>
+  );
 };
 
 export { MapPage };
