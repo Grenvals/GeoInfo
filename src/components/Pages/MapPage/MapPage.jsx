f158f26187fbe890d865225e8eea1c47a220f690
Grenvals <grenvals@gmail.com>
2020-09-04 13:00:05 +0300

    add markers, fix location

--- src/components/Pages/MapPage/MapPage.jsx
+++ src/components/Pages/MapPage/MapPage.jsx
@@ -31,9 +31,50 @@ const MapPage = () => {
     }
   }, [currentLocation]);
 
+  const markers = [
+    {
+      id: 1,
+      name: 'Pharmacy',
+      category: 'pharmacies',
+      latlng: {
+        lat: 50.430864,
+        lng: 30.415664,
+      },
+    },
+    {
+      id: 2,
+      name: 'Okko',
+      category: 'gas stations',
+      latlng: {
+        lat: 50.480864,
+        lng: 30.615664,
+      },
+    },
+    {
+      id: 3,
+      name: 'School 4',
+      category: 'schools',
+      latlng: {
+        lat: 50.540864,
+        lng: 30.415664,
+      },
+    },
+    {
+      id: 4,
+      name: 'Sacramento',
+      category: 'restaurants',
+      latlng: {
+        lat: 50.440864,
+        lng: 30.615664,
+      },
+    },
+  ];
+
   return (
     <div className="mapPage">
-      {initialSettings && <LeafletMap initialSettings={initialSettings} />}
+      {initialSettings && (
+        <LeafletMap initialSettings={initialSettings} markers={markers} />
+      )}
     </div>
   );
 };
--- src/components/common/LeafletMap/LeafletMap.jsx
+++ src/components/common/LeafletMap/LeafletMap.jsx
@@ -1,12 +1,36 @@
 import React, { useRef, useEffect, useState } from 'react';
-import L from 'leaflet';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import redMarkerIcon from '../../../assets/img/markers/red-marker.svg';
+import greenMarkerIcon from '../../../assets/img/markers/green-marker.svg';
+import cuttentLocationMarkerIcon from '../../../assets/img/markers/home-marker.svg';
 
 import './LeafletMap.scss';
 
 L.Icon.Default.imagePath = 'https://unpkg.com/leaflet@1.7.1/dist/images/';
 
-const LeafletMap = ({ initialSettings }) => {
+const redMarker = new L.Icon({
+  iconUrl: redMarkerIcon,
+  iconRetinaUrl: redMarkerIcon,
+  iconSize: new L.Point(38, 58),
+  className: 'leaflet-div-icon',
+});
+
+const greenMarker = new L.Icon({
+  iconUrl: greenMarkerIcon,
+  iconRetinaUrl: greenMarkerIcon,
+  iconSize: new L.Point(38, 58),
+  className: 'leaflet-div-icon',
+});
+
+const currentLocationMarker = new L.Icon({
+  iconUrl: cuttentLocationMarkerIcon,
+  iconRetinaUrl: cuttentLocationMarkerIcon,
+  iconSize: new L.Point(47, 58),
+  className: 'leaflet-div-icon',
+});
+
+const LeafletMap = ({ initialSettings, markers }) => {
   const mapRef = useRef();
 
   const center = [initialSettings.lat, initialSettings.lng];
@@ -19,19 +43,81 @@ const LeafletMap = ({ initialSettings }) => {
     }, 1000);
   }, [mapRef]);
 
+  const [unsaveMarkers, setUnsaveMarkers] = useState([]);
+
+  const [isMarkerEditMode, setIsMarkerEditMode] = useState(false);
+
+  const onMarkerClick = (e) => {
+    console.log(e);
+  };
+
+  const markersList = markers.map((marker) => (
+    <Marker
+      key={marker.id}
+      position={[marker.latlng.lat, marker.latlng.lng]}
+      icon={greenMarker}
+      openPopup={true}
+      onClick={() => {
+        onMarkerClick(marker);
+      }}>
+      <Popup>
+        <div className="gggg">
+          {isMarkerEditMode ? <input type="text" /> : <p>{marker.name}</p>}
+        </div>
+      </Popup>
+    </Marker>
+  ));
+
+  const localMarkers = unsaveMarkers.map((marker) => (
+    <Marker
+      key={marker.id}
+      position={[marker.latlng.lat, marker.latlng.lng]}
+      icon={redMarker}
+      openPopup={true}
+      onClick={() => {
+        onMarkerClick(marker);
+      }}>
+      <Popup>
+        <div className="gggg">
+          {isMarkerEditMode ? <input type="text" /> : <p>{marker.name}</p>}
+        </div>
+      </Popup>
+    </Marker>
+  ));
+
+  const addLocalMarker = (e) => {
+    const markers = [...unsaveMarkers];
+    const newId = `f${(+new Date()).toString(16)}`;
+    markers.push({
+      id: newId,
+      name: 'New marker',
+      category: 'another',
+      latlng: e.latlng,
+    });
+    setUnsaveMarkers(markers);
+  };
+
   return (
     <div className="map">
-      <Map zoom={initialSettings.zoom} center={center} ref={mapRef}>
+      <Map
+        zoom={initialSettings.zoom}
+        center={center}
+        ref={mapRef}
+        onClick={addLocalMarker}>
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={center}>
+        <Marker position={center} icon={currentLocationMarker}>
           <Popup>
-            <p>MainText</p>
-            <p>MainText</p>
+            <img src={cuttentLocationMarkerIcon} alt="icon" />
+            <h3>Your current location:</h3>
+            <p>latitude: {center[0].toFixed(3)}</p>
+            <p>longitude: {center[1].toFixed(3)}</p>
           </Popup>
         </Marker>
+        {markersList}
+        {localMarkers}
       </Map>
     </div>
   );
